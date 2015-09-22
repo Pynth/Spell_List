322a4f04b9993e1d5a2da2fd73d00b12b8fd9cc8
Will Lyon <wrl00510@gmail.com>
2015-09-22 04:58:59 -0500

    Adding values
    Put the drawer slots for all classes and strings for title changes

--- app/src/main/java/com/example/will/spelllistdnd/MainActivity.java
+++ app/src/main/java/com/example/will/spelllistdnd/MainActivity.java
@@ -66,6 +66,24 @@ public class MainActivity extends AppCompatActivity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
+            case 5:
+                mTitle = getString(R.string.title_section5);
+                break;
+            case 6:
+                mTitle = getString(R.string.title_section6);
+                break;
+            case 7:
+                mTitle = getString(R.string.title_section7);
+                break;
+            case 8:
+                mTitle = getString(R.string.title_section8);
+                break;
+            case 9:
+                mTitle = getString(R.string.title_section9);
+                break;
         }
     }
 
